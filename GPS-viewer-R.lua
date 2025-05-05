136ee868e7922b5dfb567ed5f68cccfe81d421b9
Lipatov <maksym_lipatov@epam.com>
2025-05-05 21:20:02 +0300

    fix names

--- GPS-viewer-R.lua
+++ GPS-viewer-R.lua
@@ -3,32 +3,32 @@ local file_exist = false
 local buffer
 local coordinates
 local linectr = 1
-local item = 1
+local scrollIndex = 1
 local string_gmatch = string.gmatch
-local displayItensLength = 5
+local displayItemsLength = 5
 -- 128x64 
 
 
 local function splitstring(text)
 
 	if text ~= nil then
-		local text_split = {} 
+		local text_split = {}
 		local i=1
 		--split by "," and store into array/table
-		for word in string_gmatch(text, "([^,]+)") do	
+		for word in string_gmatch(text, "([^,]+)") do
 			text_split[i] = word
-			i = i + 1			
-		end	
+			i = i + 1
+		end
 
 		return text_split
-	end 
+	end
 end
 
 local function getCurrentElements()
     local arr = {}
 
-    for i=1, displayItensLength  do
-        local coordinate = coordinates[item-1+i]
+    for i=1, displayItemsLength do
+        local coordinate = coordinates[scrollIndex -1+i]
         if coordinate ~= nil then
             arr[i] = coordinate
         end
@@ -38,11 +38,11 @@ end
 
 local function Viewer_Draw_LCD(i)
 
-    lcd.clear() 	
-	lcd.drawLine(0,0,0,64, SOLID, FORCE)	
+    lcd.clear()
+	lcd.drawLine(0,0,0,64, SOLID, FORCE)
 	lcd.drawLine(127,0,127,64, SOLID, FORCE)
-	lcd.drawText(2,1,"GPS stats viewer v1.2" ,SMLSIZE)	
-    lcd.drawLine(0,63,127,63, SOLID, FORCE)	
+	lcd.drawText(2,1,"GPS stats viewer v1.2" ,SMLSIZE)
+    lcd.drawLine(0,63,127,63, SOLID, FORCE)
     lcd.drawFilledRectangle(1,0, 126, 9, GREY_DEFAULT)
 	--lcd.drawFilledRectangle(1,0, 126, 9, GREY_DEFAULT)
 
@@ -58,37 +58,37 @@ end
 
 local function Viewer_Init()
 	coordinates = {}
-    item = 1
-	lcd.clear() 
-	local f2 = io.open(log_filename, "r")	
+    scrollIndex = 1
+	lcd.clear()
+	local f2 = io.open(log_filename, "r")
 	linectr = 1
-	--check if file exists 
+	--check if file exists
 	if f2 ~= nil then
-			
+
 		file_exist = true
 		buffer = io.read(f2, 4096)
 		io.close(f2)
 
 		--read file contents into array/table
-		for line in string_gmatch(buffer, "([^\n]+)\n") do			
+		for line in string_gmatch(buffer, "([^\n]+)\n") do
 			if not string.find(line, "Number")  then --exclude logfile headline
                 local first_comma_index = string.find(line, ",")
                 local new_str = tostring(linectr) .. string.sub(line, first_comma_index)
-				coordinates[linectr] = new_str	
-				linectr = linectr + 1		
+				coordinates[linectr] = new_str
+				linectr = linectr + 1
 			end
-		end			
-	
+		end
+
 		--draw inital screen
 		Viewer_Draw_LCD(1)
-				
+
 	else
-		file_exist = false		
+		file_exist = false
 	end
  end
 
 
- 
+
 -- Main
 local function Viewer_Run(event)
 
@@ -97,24 +97,22 @@ local function Viewer_Run(event)
 		return 2
 	else
 
-        if file_exist == true then	
-            Viewer_Draw_LCD(item)  
-            if event == EVT_VIRTUAL_INC  then  
-                
-				item = item + 1
-			end		
-	
-			if event == EVT_VIRTUAL_DEC  then      
-				item = item - 1				
+        if file_exist == true then
+            Viewer_Draw_LCD(scrollIndex)
+            if event == EVT_VIRTUAL_INC  then
+                scrollIndex = scrollIndex + 1
 			end
-            if item > linectr - displayItensLength then
-                item = linectr - displayItensLength
-            end
-            if item < 1 then
-                item = 1
-            end
 
-            
+			if event == EVT_VIRTUAL_DEC  then
+				scrollIndex = scrollIndex - 1
+
+			end
+            if scrollIndex > linectr - displayItemsLength then
+                scrollIndex = linectr - displayItemsLength
+            end
+            if scrollIndex < 1 then
+                scrollIndex = 1
+            end
         end
         if event == EVT_VIRTUAL_EXIT then
 			return 2
